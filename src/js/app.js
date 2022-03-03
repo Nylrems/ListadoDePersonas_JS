a8cbad6d8e48fa0a37853fbdfa01084a34afe506
Slermyn <smerlyn_alejandro@outlook.com>
2022-03-03 14:37:54 -0400

    App terminada

--- src/js/app.js
+++ src/js/app.js
@@ -1,14 +1,30 @@
-const personas = [
-    new Persona('Juan', 'Pérez'),
-    new Persona('Anna', 'Pérez'),
-];
+const personas = [new Persona("Juan", "Pérez"), new Persona("Anna", "Pérez")];
 
-const mostrarPersonas = () =>{
-    console.log('Mostrar persona');
-    let texto = '';
-    for(let persona of personas){
-        console.log(persona);
-        texto += `<li> - ${persona.nombre} ${persona.apellido}</li>`;
-    }
-    document.getElementById('personas').innerHTML = texto;
-}
+const mostrarPersonas = () => {
+  console.log("Mostrar persona");
+  let texto = "";
+  for (let persona of personas) {
+    console.log(persona);
+    texto += `<li> - ${persona.nombre} ${persona.apellido}</li>`;
+  }
+  document.getElementById("personas").innerHTML = texto;
+};
+
+const agregarPersona = () => {
+  const forma = document.forms["forma"];
+  const nombre = forma["nombre"];
+  const apellido = forma["apellido"];
+  if (nombre.value != "" && apellido.value != "") {
+    const persona = new Persona(nombre.value, apellido.value);
+    console.log(persona);
+    personas.push(persona);
+    mostrarPersonas();
+
+    nombre.value = "";
+    apellido.value = "";
+  }
+  else{
+      console.log('Suministre información');
+      
+  }
+};
